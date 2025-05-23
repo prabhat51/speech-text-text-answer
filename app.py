892634c2e6734d353e89bae53365391fa45cafb9
prabhat51 <88582253+prabhat51@users.noreply.github.com>
2025-05-23 20:25:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ def upload_audio():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
